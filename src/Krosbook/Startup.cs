06a15e6cd1a7cfb9032f7c386a2278c4c4905fb5
ivansopoliga <i.sopoliga@gmail.com>
2016-07-04 13:28:17 +0200

    Initial commit

--- src/Krosbook/Startup.cs
+++ src/Krosbook/Startup.cs
@@ -47,14 +47,10 @@ namespace Krosbook
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseDefaultFiles();
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
         }
     }
 }
