109c527b19e281930267da477c504464ef4797de
ivansopoliga <i.sopoliga@gmail.com>
2016-07-04 14:44:22 +0200

    Angular

---
+++ src/Krosbook/Scripts/main.ts
@@ -0,0 +1,11 @@
+﻿import {disableDeprecatedForms, provideForms} from '@angular/forms';
+import {bootstrap}    from '@angular/platform-browser-dynamic';
+
+import {AppComponent} from './components/app.component';
+
+import 'rxjs/Rx';
+
+bootstrap(AppComponent, [
+    disableDeprecatedForms(),
+    provideForms()] 
+);
---
+++ src/Krosbook/gulpfile.js
@@ -0,0 +1,73 @@
+﻿/// <binding AfterBuild="build" Clean="clean" />
+"use strict";
+
+var gulp = require("gulp"),
+    rimraf = require("gulp-rimraf"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify"),
+    sass = require("gulp-sass"),
+    typescript = require("gulp-typescript");
+
+var paths = {
+    webroot: "./wwwroot/",
+    npm: "./node_modules/",
+    lib: "./wwwroot/lib/",
+    scripts: "./Scripts/",
+    app: "./wwwroot/app/"
+};
+
+var libs = [
+    paths.npm + "@angular2/**/*.js",
+    paths.npm + "systemjs/**/*.js"
+];
+
+gulp.task("rxjs", function () {
+    return gulp.src(paths.npm + "rxjs/**/*.js")
+        .pipe(gulp.dest(paths.lib + "rxjs/"));
+});
+
+gulp.task("anuglar", function () {
+    return gulp.src(paths.npm + "@angular/**/*.js")
+        .pipe(gulp.dest(paths.lib + "@angular/"));
+});
+
+gulp.task("systemjs", function () {
+    return gulp.src(paths.npm + "systemjs/**/*.js")
+        .pipe(gulp.dest(paths.lib + "systemjs/"));
+});
+
+gulp.task("libs", ["rxjs", "anuglar", "systemjs"]);
+
+gulp.task("clean", function () {
+    return gulp.src(paths.app + "**/*.*", { read: false })
+        .pipe(rimraf());
+});
+
+gulp.task("html", function () {
+    return gulp.src(paths.scripts + "**/*.html")
+        .pipe(gulp.dest(paths.app));
+});
+
+gulp.task("js", function () {
+    return gulp.src(paths.scripts + "**/*.ts")
+        .pipe(typescript({
+                "noImplicitAny": false,
+                "noEmitOnError": true,
+                "removeComments": false,
+                "sourceMap": false,
+                "target": "es5",
+                "module": "commonjs",
+                "experimentalDecorators": true,
+                "emitDecoratorMetadata": true
+        }))
+        .pipe(gulp.dest(paths.app));
+});
+
+gulp.task("css", function () {
+    return gulp.src(paths.scripts + "**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest(paths.app));
+});
+
+gulp.task("build", ["libs", "html", "js", "css"]);
