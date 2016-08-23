9e5061aef13cb60d55018925edc447126116d66f
Ondrej Pudiš <ondrejpudis@gmail.com>
2016-08-23 11:29:10 +0200

    Services & loading users names in reservation

--- src/Krosbook/Scripts/app/services/reservation.service.ts
+++ src/Krosbook/Scripts/app/services/reservation.service.ts
@@ -1,14 +1,8 @@
-/**
- * Created by Ondrej on 01.08.2016.
- */
 import {Injectable} from '@angular/core';
 import {Http, Headers} from '@angular/http';
-import 'rxjs/add/operator/map'
-import {Observable} from "rxjs/Rx";
 
 @Injectable()
 export class ReservationService {
-
   constructor(private http: Http) {
     let _build = (<any>http)._backend._browserXHR.build;
     (<any>http)._backend._browserXHR.build = () => {
@@ -21,14 +15,14 @@ export class ReservationService {
   public getReservation(type:string, id:number){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.get('/api/reservations/'+type+'/'+id, {headers});
+    return this.http.get('http://funnybean.cloudapp.net/api/reservations/'+type+'/'+id, {headers});
   }
 
   public getReservations(type:string, id:number, from:any, to:any){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     var by = (type == "rooms") ? "byroom" : "bycar";
-    return this.http.post('/api/reservations/'+type+'/'+by+'/'+id, JSON.stringify({from, to}), {headers});
+    return this.http.post('http://funnybean.cloudapp.net/api/reservations/'+type+'/'+by+'/'+id, JSON.stringify({from, to}), {headers});
   }
 
   public addReservation(type:string, elementId: number, userId:number, name:string, date:string, length:number){
@@ -36,28 +30,28 @@ export class ReservationService {
     headers.append('Content-Type', 'application/json');
     if(type == "rooms"){
       var roomId = elementId;
-      return this.http.post('/api/reservations/'+type+'/', JSON.stringify({roomId, name, date, length}), {headers});
+      return this.http.post('http://funnybean.cloudapp.net/api/reservations/'+type+'/', JSON.stringify({roomId, name, date, length}), {headers});
     } else {
       var carId = elementId;
-      return this.http.post('/api/reservations/'+type+'/', JSON.stringify({carId, name, date, length}), {headers});
+      return this.http.post('http://funnybean.cloudapp.net/api/reservations/'+type+'/', JSON.stringify({carId, name, date, length}), {headers});
     }
   }
 
-  public editReservation(type: string, id: number, name: string, elementId: number, userId: number, dateTime:any, length:number, emailInvitation:boolean, goToMeeting:boolean){
+  public editReservation(type:string, id:number, name:string, elementId:number, userId:number, dateTime:any, length:number, emailInvitation:boolean, goToMeeting:boolean){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     if(type == "rooms"){
-        var roomId = elementId;
-        return this.http.put('/api/reservations/' + type + '/' + id, JSON.stringify({ id, name, roomId, dateTime, userId, length, emailInvitation, goToMeeting}), {headers});
+      var roomId = elementId;
+      return this.http.put('http://funnybean.cloudapp.net/api/reservations/'+type+'/'+id, JSON.stringify({id, name, roomId, dateTime, userId, length, emailInvitation, goToMeeting}), {headers});
     } else {
       var carId = elementId;
-      return this.http.put('/api/reservations/' + type + '/' + id, JSON.stringify({ id, name, carId, dateTime, userId, length}), {headers});
+      return this.http.put('http://funnybean.cloudapp.net/api/reservations/'+type+'/'+id, JSON.stringify({id, name, carId, dateTime, userId, length}), {headers});
     }
   }
 
   public deleteReservation(type:string, id:number){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.delete('/api/reservations/'+type+'/'+id, {headers});
+    return this.http.delete('http://funnybean.cloudapp.net/api/reservations/'+type+'/'+id, {headers});
   }
 }
--- src/Krosbook/Scripts/app/services/roles.service.ts
+++ src/Krosbook/Scripts/app/services/roles.service.ts
@@ -1,11 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Http, Headers} from '@angular/http';
-import 'rxjs/add/operator/map'
-import {Observable} from "rxjs/Rx";
 
 @Injectable()
 export class RolesService {
-
   constructor(private http: Http) {
     let _build = (<any>http)._backend._browserXHR.build;
     (<any>http)._backend._browserXHR.build = () => {
@@ -18,29 +15,29 @@ export class RolesService {
   public getRoles(){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.get('/api/roles', { headers  } );
+    return this.http.get('http://funnybean.cloudapp.net/api/roles', { headers  } );
   }
 
   public getRole(id:number){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.get('/api/roles/'+id, { headers  } );
+    return this.http.get('http://funnybean.cloudapp.net/api/roles/'+id, { headers  } );
   }
 
   public addRole(role:string) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('/api/roles',role, {headers});
+    return this.http.post('http://funnybean.cloudapp.net/api/roles',role, {headers});
   }
 
   public editRole(id:number, role:string){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.put("/api/roles/"+id, role, {headers});
+    return this.http.put("http://funnybean.cloudapp.net/api/roles/"+id, role, {headers});
   }
 
   public removeRole(id: string) {
-    return this.http.delete('/api/roles/'+id);
+    return this.http.delete('http://funnybean.cloudapp.net/api/roles/'+id);
   }
 
 
