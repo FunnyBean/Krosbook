787cf392070782419481e6a1dffe4bda7ad9fa75
majco333 <majco333@gmail.com>
2016-08-23 11:16:58 +0200

    changes with gulp + prodMode

--- src/Krosbook/wwwroot/main.js
+++ src/Krosbook/wwwroot/main.js
@@ -1,7 +1,9 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var core_1 = require('@angular/core');
 var app_component_1 = require('./app/components/app.component');
 var app_routes_1 = require('./app/components/app.routes');
 var http_1 = require('@angular/http');
 var forms_1 = require('@angular/forms');
+core_1.enableProdMode();
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [forms_1.disableDeprecatedForms(), forms_1.provideForms(), app_routes_1.APP_ROUTER_PROVIDERS, http_1.HTTP_PROVIDERS]);
